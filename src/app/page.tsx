eae061b01ad963e9b707dcf148b1d1163f6c77ac
Scriptscrypt <tirumaladasa@gmail.com>
2024-10-19 14:31:59 +0530

    Feat: Init sidebar #2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { SidebarComp } from "./app-ui-components/SidebarComp";
+
 import { Sidebar, SidebarProvider } from "@/components/ui/sidebar";
 import React from "react";
 
@@ -9,13 +9,6 @@ export default function Home() {
   return (
    <>
    <div className="flex gap-4">
-
-   <div className="">LEFT
-
-
-   <SidebarComp/>
-
-   </div>
    
    <div className="">CENTER</div>
 
